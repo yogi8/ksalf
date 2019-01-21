50232c675e28746cd7b04cef3e615e172cf497a5
yogi8 <yogeeswarreddyp@gmail.com>
2019-01-21 15:39:40 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,8 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-return ('Hello, World!')
-=======
-    return ('Hello, World!')
->>>>>>> d34230fc2b386830c9fc677a59a154b66b5af123
+    print ('hi')
