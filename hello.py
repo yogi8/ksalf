2d1821f0e32a055902d73cea5f03808bdee6bf38
yogi8 <yogeeswarreddyp@gmail.com>
2019-01-22 11:20:44 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return ('hi')
+
+stores = [
+           {'name': 'My Store',
+           'items': [{'name': 'my item',
+                      'price': 15.99}
+                     ]
+           }
+          ]
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+
+
 if __name__ == '__main__':
  app.run(host='0.0.0.0',port=5000, debug=True)
