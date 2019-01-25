f4ecee4d12c77a28c93da4feb0d6b8ad93d12d23
yogi8 <yogeeswarreddyp@gmail.com>
2019-01-25 18:18:07 +0530

    Create model.py

---
+++ modelapp/models/model.py
@@ -0,0 +1,21 @@
+from db import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    eid = db.Column(db.Integer)
+    en = db.Column(db.String(200))
+    ad = db.Column(db.String(200))
+    ga = db.Column(db.String(200))
+    ba = db.Column(db.String(200))
+
+    def __init__(self, eid, en, ad, ga, ba):
+        self.eid = eid
+        self.en = en
+        self.ad = ad
+        self.ga = ga
+        self.ba = ba
+
+    def json(self):
+        return {'name': self.eid}
