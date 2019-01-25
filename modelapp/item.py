a254278f9076b1a417bfb5896be069f6dc66d34d
yogi8 <yogeeswarreddyp@gmail.com>
2019-01-25 17:06:07 +0530

    Update item.py

--- modelapp/item.py
+++ modelapp/item.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, reqparse
+from flask import request
 class Item(Resource):
     parser = reqparse.RequestParser()
-
     def post(self):
-        data = Item.parser.parse_args()
-        return data
+        data = request.data['value']
+        yogi =  {'yogi': data}
+        return yogi
