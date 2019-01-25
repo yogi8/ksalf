efe4cf19dfb681d30c6382a2e081b7db8b540731
yogi8 <yogeeswarreddyp@gmail.com>
2019-01-25 18:25:01 +0530

    Update app.py

--- modelapp/app.py
+++ modelapp/app.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 api.add_resource(Item, '/item')
 
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(host='0.0.0.0', port=5000, debug=True)
